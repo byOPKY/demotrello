b6559d205e922557336f459229a6d55a7b055bb5
byOPKY <orrillokarol@gmail.com>
2016-09-22 18:31:45 -0500

    elimine error en el cuadro dos

--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@ window.onload = function () {
 			agregar.classList.add("no");
 			none.classList.remove("no"); 
 			none.classList.add("si"); 
+
 	});
 	none.addEventListener("mouseover", function() {
 			agregar.classList.add("no");
@@ -38,10 +39,45 @@ window.onload = function () {
 		title.classList.add("titulo");
 		cuadro.insertBefore(title, cuadro.childNodes[0]);
 
-		var input = document.createElement("input");
+		var input = document.createElement("a");
 		cuadro.insertBefore(input, cuadro.childNodes[1]);
 		escribir.value= "";
-	});
+		var link = document.createTextNode("Añadir una Lista");
+		input.appendChild(link);
+
+		input.addEventListener("click" ,function(){
+			var cuadro2= document.createElement("div");
+			cuadro2.classList.add("cuadro2");
+			cuadro2.classList.add("border");
+			cuadro.appendChild(cuadro2);
+			input.classList.add("no")
+			var aText = document.createElement("textarea")
+			cuadro2.insertBefore(aText, cuadro2.childNodes[0]);
+			
+			var aBoton = document.createElement("button");
+			aBoton.classList.add("aBoton")
+			aBoton.classList.add("border")
+			cuadro2.insertBefore(aBoton, cuadro2.childNodes[1]);
+			var linkBoton = document.createTextNode("Guardar");
+			aBoton.appendChild(linkBoton);
+			
+			aBoton.addEventListener("click", function(){
+				var context = aText.value;
+				var textContext=document.createElement("div");
+				textContext.innerHTML = context;
+				textContext.classList.add("textos");
+				cuadro.insertBefore(textContext,cuadro.childNodes[1]);
+				aText.value ="";
+				cuadro2.classList.add("no"); 
+				input.classList.remove("no")
+
+			});
+			agregar.addEventListener("click", function() {
+				input.classList.add("si")});
+			});
+		
+		});
+	
 	
 }
 	/*  
